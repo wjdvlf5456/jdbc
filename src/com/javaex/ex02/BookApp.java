86f1da45063e2ee63db3ce2ad9799d0d9061bfc2
최정필 <wjdvlf1407@gmail.com>
2022-05-23 17:13:43 +0900

    책 insert 데이터

--- src/com/javaex/ex02/BookApp.java
+++ src/com/javaex/ex02/BookApp.java
@@ -8,6 +8,12 @@ public class BookApp {
 
 		bookAdo.bookInsert("우리들의 일그러진 영웅", "다림", "1998-02-22", 1);
 		bookAdo.bookInsert("삼국지", "민음사", "2002-03-01", 1);
+		bookAdo.bookInsert("토지", "마로니에북스", "2012-08-15", 2);
+		bookAdo.bookInsert("유시민의 글쓰기 특강", "생각의 길", "2015-04-01", 3);
+		bookAdo.bookInsert("패션왕", "중앙북스(books)", "2012-02-22", 4);
+		bookAdo.bookInsert("순정만화", "재미주의", "2011-08-03", 5);
+		bookAdo.bookInsert("오직두사람", "문학동네", "2017-05-04", 6);
+		bookAdo.bookInsert("26년", "재미주의", "2012-02-04", 5);
 
 	}
 
